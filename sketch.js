c3cc25d9331588ded026e0991564c76550359a91
Mayan27 <122705591+Mayan27@users.noreply.github.com>
2023-02-05 11:41:51 +1100

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -25,9 +25,9 @@ boy.addAnimation("JakeRunning",boyImg);
 
 leftBoundary=createSprite(0,0,100,800);
 
-leftBoundary.invisible = false;
- leftBoundary.visible = true;
-leftBoundary.invisible = true;
+//leftBoundary.invisible = false;
+ //leftBoundary.visible = true;
+//leftBoundary.invisible = true;
  leftBoundary.visible = false;
 
 
@@ -48,9 +48,9 @@ function draw() {
   
   //code to reset the background
 
-  /*if(path.y > 400 ){
+  if(path.y > 400 ){
     path.y = height/2;
-  }*/
+  }
 
   /*if(path.y > 400 ){
    
